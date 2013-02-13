283e54b504ff5e241fc38e81b57a59ba3883f6fb
Héctor L <hlaura@hlaura.(none)>
2013-02-13 11:17:02 +0100

    Corregido bug que sustituia el path en la URL en todas las ocurrencias y no solo en la primer

--- application/php/libs/class.Application.php
+++ application/php/libs/class.Application.php
@@ -185,7 +185,8 @@ class Application {
 
 		$uri = array('controller' => self::DEFAULT_CONTROLLER, 'method' => '', 'var' => '');
 
-		$url = str_replace(SITE_PATH, '', $url);
+		$path = str_replace("/", "\\/", SITE_PATH);
+		$url = preg_replace('/\/' . $path .'/', '', $url, 1);
 
 		$array_tmp_uri = preg_split('[\\/]', $url, -1, PREG_SPLIT_NO_EMPTY);
 
